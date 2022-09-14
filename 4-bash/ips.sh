6abdc57fa1d482249c5b34f430e327b087d6212d
Darrell Tang <darrelltang@gmail.com>
2022-09-14 13:27:38 -0700

    update comments with sources

--- 4-bash/ips.sh
+++ 4-bash/ips.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Check if a filename was passed as a parameter
 # A sample file can be found in sample.log in this directory
+# sample.log consists of the first 10k lines from https://www.kaggle.com/datasets/kimjmin/apache-web-log
 if [ $# -eq 0 ]
   then
     echo "Please provide a filename. i.e. ./ips.sh filename.log"
@@ -14,4 +15,5 @@ filename=$1
 # count unique occurrences
 # reverse sort the list
 # output top 10 lines
+# Regex for ipv4 found here: https://www.putorius.net/grep-an-ip-address-from-a-file.html
 grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $filename | sort | uniq -c | sort -r | head
